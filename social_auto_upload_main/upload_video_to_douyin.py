1da18927b9a9d549a7927bc8fe83253bed0ba126
yinke <yinke@eastmoney.com>
2024-05-23 17:32:09 +0800

    fix cover logic

--- social_auto_upload_main/upload_video_to_douyin.py
+++ social_auto_upload_main/upload_video_to_douyin.py
@@ -34,7 +34,7 @@ def upload_video(args):
     # 获取视频目录
     folder_path = Path(filepath)
     # 获取文件夹中的所有文件
-    files = list(folder_path.glob("*.mp4"))
+    files = list(folder_path.glob("video.mp4"))
     file_num = len(files)
     if file_num == 0:
         logger.info(f"No video found in {folder_path}...")
@@ -44,7 +44,7 @@ def upload_video(args):
     # cookie_setup = asyncio.run(douyin_setup(account_file, handle=False))
     for index, file in enumerate(files):
         title, tags = get_title_and_hashtags(str(file))
-        cover_file_path = str(file).replace(".mp4",".jpg")
+        cover_file_path = str(file).replace("video.mp4","cover.jpg")
         # 打印视频文件名、标题和 hashtag
         print(f"视频文件名：{file}")
         print(f"标题：{title}")
