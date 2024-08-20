0cda0579532354f3fbf9669b52018df44c34ec8b
yinke <weirdoyinke@gmail.com>
2024-08-20 17:14:56 +0800

    add preview

--- README.md
+++ README.md
@@ -1,24 +1,9 @@
 # optimus
-[ ] 文本分割
+
 
 
 # Setup
 * install [ffmpeg](https://ffmpeg.org/download.html)
   * for [linux](https://json2video.com/how-to/ffmpeg-course/install-ffmpeg-linux.html)
   * for [windows](https://blog.csdn.net/csdn_yudong/article/details/129182648)
-* install [torch](https://pytorch.org/get-started/locally/)
-
-## GPT_SoVITS
-使用不同的tts模型：
-只需更改：
-`GPT_SoVITS_main/gweight.txt`和`GPT_SoVITS_main/sweight.txt`
-
-如真白花音的模型：
-gweight.txt：
-GPT_SoVITS/pretrained_models/BAICAI-JA-e10.ckpt
-
-sweight.txt：
-GPT_SoVITS/pretrained_models/BAICAI-JA_e70_s11410.pth
-
-
-如果想用重新使用默认模型的话，请将gweight.txt和sweight.txt删除
+* pip install -r requirements
--- pipeline_v2.py
+++ pipeline_v2.py
@@ -3,16 +3,18 @@ from optimus_tools.log_utils import get_logger
 
 from optimus_tools.ffmpeg_utils import merge_video_audio, add_subtile, make_cover, change_video_speed
 from optimus_tools.text2image_utils import subtitle2video, calculate_image_duration, concat_images_to_video
+from optimus_tools.coze_api import preview_shot_info
 from functools import reduce
 
 logger = get_logger(__name__)
 from optimus_tools.asure_cognition import gen_subtile, text2speech_with_timestamp
 
 def img2vid_from_coze_outputs(curr_work_dir):
+    preview_shot_info(curr_work_dir)
+    input("please preview the preview.html... 按任意键继续....")
     text2speech_with_timestamp(curr_work_dir)
     gen_subtile(curr_work_dir)
     concat_images_to_video(curr_work_dir)
-
     merge_video_audio(curr_work_dir)
     add_subtile(curr_work_dir)
     change_video_speed(curr_work_dir)
